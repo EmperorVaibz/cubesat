f7845bd63fb653e5099c586b942e92672cf3db68
EmperorVaibz <65975752+EmperorVaibz@users.noreply.github.com>
2024-01-20 12:48:53 -0800

    Create basic_imaging.py

---
+++ Code/basic_imaging.py
@@ -0,0 +1,85 @@
+"""
+The Python code you will write for this module should read
+acceleration data from the IMU. When a reading comes in that surpasses
+an acceleration threshold (indicating a shake), your Pi should pause,
+trigger the camera to take a picture, then save the image with a
+descriptive filename.
+"""
+
+#AUTHOR: XCubed
+#DATE: January 20, 2024
+
+#import libraries
+import board
+import time
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
+from adafruit_lis3mdl import LIS3MDL
+from git import Repo
+from picamera2 import Picamera2
+
+#VARIABLES
+THRESHOLD = 0      #Any desired value from the accelerometer
+REPO_PATH = "https://github.com/EmperorVaibz/cubesat.git"
+FOLDER_PATH = "/Images"
+
+#imu and camera initialization
+i2c = board.I2C()
+accel_gyro = LSM6DS(i2c)
+mag = LIS3MDL(i2c)
+picam2 = Picamera2()
+
+
+def git_push():
+    """
+    This function is complete. Stages, commits, and pushes new images to your GitHub repo.
+    """
+    try:
+        repo = Repo(REPO_PATH)
+        origin = repo.remote('origin')
+        print('added remote')
+        origin.pull()
+        print('pulled changes')
+        repo.git.add(REPO_PATH + FOLDER_PATH)
+        repo.index.commit('New Photo')
+        print('made the commit')
+        origin.push()
+        print('pushed changes')
+    except:
+        print('Couldn\'t upload to git')
+
+
+def img_gen(name):
+    """
+    This function is complete. Generates a new image name.
+
+    Parameters:
+        name (str): ex. obsImages
+    """
+    t = time.strftime("_%H%M%S")
+    imgname = (f'{REPO_PATH}/{FOLDER_PATH}/{name}{t}.jpg')
+    return imgname
+
+
+def take_photo():
+    """
+    This function is NOT complete. Takes a photo when the FlatSat is shaken.
+    Replace psuedocode with your own code.
+    """
+    while True:
+        accelx, accely, accelz = accel_gyro.acceleration
+        if(accelx>0 or accely>0 or accelz>0):
+        
+            sleep(2)
+            name = img_gen("basic_image")
+            picam2.capture(name+'.jpg')
+            git_push()
+        
+        sleep(2)
+
+
+def main():
+    take_photo()
+
+
+if __name__ == '__main__':
+    main()
